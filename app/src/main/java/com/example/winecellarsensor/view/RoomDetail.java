26d3b474b0f8a0496b4bb46944230ee52afe80d7
rorienn <45061885+rorienn@users.noreply.github.com>
2019-12-11 12:06:18 +0100

    Date formatting Gson configuration

--- app/src/main/java/com/example/winecellarsensor/view/RoomDetail.java
+++ app/src/main/java/com/example/winecellarsensor/view/RoomDetail.java
@@ -3,6 +3,7 @@ package com.example.winecellarsensor.view;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.winecellarsensor.R;
 
@@ -10,11 +11,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class RoomDetail extends AppCompatActivity {
 
+    private Button buttonStatistics;
+    private Button buttonSettings;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room_detail);
 
+        buttonSettings = findViewById(R.id.btn_Statistics);
+        buttonStatistics = findViewById(R.id.btn_Settings);
+
+        buttonStatistics.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showStatistics(view);
+            }
+        });
+
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSettings(view);
+            }
+        });
 
     }
 
